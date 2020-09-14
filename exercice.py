52e6b6830d4babba7e40a7eb2c96378a62569176
LaurentFaucher <laurent.faucher@polymtl.ca>
2020-09-14 20:35:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,11 +26,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 def to_celsius(temperature: float) -> float:
     return (temperature - 32) / 1.8
-    
+
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return ()
 
 
 def main() -> None:
